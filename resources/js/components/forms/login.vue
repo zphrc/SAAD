9e9ef539c448d3d107daac28ce52e39f4e1f2aea
zphrc <143074336+zphrc@users.noreply.github.com>
2024-06-25 00:56:24 +0800

    Add files via upload

--- resources/js/components/forms/login.vue
+++ resources/js/components/forms/login.vue
@@ -1,6 +1,32 @@
 <template>
   <div>
-    <b-table striped hover :items="items" :fields="fields"></b-table>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          placeholder="Password"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="button" variant="primary" @click="register()">Sign Up</b-button>
+    </b-form>
   </div>
 </template>
 
@@ -8,22 +34,38 @@
   export default {
     data() {
       return {
-        items: null,
-        fields: ['id', 'USERNAME']
+        form: {
+          email: '',
+          password: '',
+        }
       }
     },
     methods:{
-      get_accounts(){
-        axios.get('/get_accounts').then(response=>{
-          console.log(response.data);
-          this.items = response.data;
+      register(){
+          axios.post('/register/registration',this.form).then(response=>{
+          console.log(response.data.email);
+          console.log(response.data.password);
         }).catch(error=>{
           console.log(error.data);
+          this.loading = false;
+        })
+      },
+      
+      onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+
+      onReset(event) {
+        event.preventDefault()
+        this.form.email = ''
+        this.form.name = ''
+        this.form.checked = []
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
         })
       }
-    },
-    beforeMount(){
-      this.get_accounts();
     }
   }
 </script>
